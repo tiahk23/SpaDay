feb8016d17d49c4c89951d9f6481b81cea0e37d6
Tiah Keefe <tetered12@gmail.com>
2021-09-09 13:15:35 -0500

    made use of the form to display proper results to the user.

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -55,6 +55,10 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+            ViewBag.name = name;
+            ViewBag.skintype = skintype;
+            ViewBag.appropriateFacials = appropriateFacials;
+            ViewBag.manipedi = manipedi;
             return View();
         }
 
